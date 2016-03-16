79917dc0984fe2ac05e6490348e45df80716c1be
Fredy Eliot Cáceres Martínez <fredye.caceres@gmail.com>
2016-03-15 23:53:24 -0500

    Support for extracting a day part object from a string

--- src/main/java/org/example/expression/parser/ExpressionParser.java
+++ src/main/java/org/example/expression/parser/ExpressionParser.java
@@ -1,6 +1,8 @@
 package org.example.expression.parser;
 
+import org.example.expression.model.DayPart;
 import org.example.expression.model.TargetingExpression;
+import org.example.expression.model.enums.DayOfWeek;
 
 /**
  * Created by fREDY on 15/03/2016.
@@ -26,6 +28,24 @@ public class ExpressionParser {
 
 
     public TargetingExpression parse(String expression) {
-        return null;
+        Integer[] adHours;
+        DayOfWeek[] daysOfWeek;
+        String[] parts = expression.split("AND");
+        String days = parts[0];
+        String hours = parts[1];
+        String daysString = days.split("IN")[1].replace("(","").replace(")","");
+        String[] dayValues = daysString.split(",");
+        String hoursString = hours.split("IN")[1].replace("(","").replace(")","");
+        String[] hourValues = hoursString.split(",");
+        adHours = new Integer[hourValues.length];
+        daysOfWeek = new DayOfWeek[dayValues.length];
+        for (int index = 0; index < dayValues.length; index++) {
+            daysOfWeek[index] = DayOfWeek.valueOf(dayValues[index].replace(" ","").replace("\"",""));
+        }
+        for (int index  = 0; index < hourValues.length; index++) {
+            adHours[index] = Integer.parseInt(hourValues[index].replace(" ","").replace("\"",""));
+        }
+        DayPart dayPart = new DayPart(daysOfWeek, adHours);
+        return new TargetingExpression(dayPart);
     }
 }
--- src/test/java/org/example/expression/parser/ExpressionParserTest.java
+++ src/test/java/org/example/expression/parser/ExpressionParserTest.java
@@ -1,6 +1,8 @@
 package org.example.expression.parser;
 
+import org.example.expression.model.DayPart;
 import org.example.expression.model.TargetingExpression;
+import org.example.expression.model.enums.DayOfWeek;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,7 +21,10 @@ public class ExpressionParserTest {
     private TargetingExpression expected;
     @Before
     public void setUp() {
-
+        DayOfWeek[] days = {DayOfWeek.Mon, DayOfWeek.Wed, DayOfWeek.Fri, DayOfWeek.Sun};
+        Integer[] hours = {4, 5, 6, 7, 8, 16, 17, 18, 19, 20};
+        DayPart dayPart = new DayPart(days, hours);
+        targetingExpression = new TargetingExpression(dayPart);
     }
 
     @Test
